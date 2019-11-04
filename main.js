7b45fd97c5f7246f4f5c82fbc628aaefd10a88fe
Law <michaellaw@gmx.com>
2019-11-04 16:07:08 -0500

    Added play and pause buttons

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 let playBtn = document.getElementById('play-btn')
+let pauseBtn = document.getElementById('pause-btn')
 let circle = document.getElementById('circle')
 
 playBtn.addEventListener('click', () => {
   circle.setAttribute('class', 'move')
+  circle.style.webkitAnimationPlayState = 'running'
+  setTimeout(() => {
+    circle.classList.remove('move')
+  }, 6000)
 })
+
+pauseBtn.addEventListener('click', () => {
+  circle.style.webkitAnimationPlayState = 'paused'
+})
+
