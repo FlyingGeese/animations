73f7e5928de53393289dcdee3ec1e2f5f2766e12
Law <michaellaw@gmx.com>
2019-11-04 16:33:20 -0500

    used grid for the street

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let playBtn = document.getElementById('play-btn')
 let pauseBtn = document.getElementById('pause-btn')
 let circle = document.getElementById('circle')
-let lines = document.getElementsByClassName('line')
+let street = document.getElementsById('street')
+let lines = street.children
 
 playBtn.addEventListener('click', () => {
   circle.setAttribute('class', 'move')
@@ -10,7 +11,7 @@ playBtn.addEventListener('click', () => {
     circle.classList.remove('move')
   }, 6000)
 
-  lines[0].classList.add('move')
+  street.classList.add('move')
 })
 
 pauseBtn.addEventListener('click', () => {
